594f9ecf912d8c527273ffa6dc20c1242757c1ef
Daria Osypova <142753291+Askada21@users.noreply.github.com>
2026-09-26 21:11:19 +0100

    Learned Strings

---
+++ harvard_strings.py
@@ -0,0 +1,22 @@
+# Ask the user for their name
+#name = input("What is your name? ")
+# Not creating a new line after first print statement 
+#print("hello, ", end="")
+#print(name)
+# Remove whitespace from the str 
+#name = name.strip() # strip() removes all whitespaces only from the beginnig and the end. 
+# Capitalize the first letter of each word
+#name = name.title()
+# Shorter form
+#name = name.strip().title()
+
+# Modify the code to be efficient 
+name = input("What's your name? ").strip().title()
+
+# Formatting String
+print(f"Hello, {name}")
+
+# When u wanna write quotations inside the print() function
+#print("hello, \"friend\"") 
+
+
